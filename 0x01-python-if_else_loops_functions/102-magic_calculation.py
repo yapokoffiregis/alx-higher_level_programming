0ed9e5c589b039709cdc0551f0084c4bdb0cea23
yapokoffiregis <regisbergan@gmail.com>
2022-08-23 21:38:59 -0700

    task number 16 commit

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b, c):
-    #Match bytecode provided by Holberton School.
     if a < b:
-        return (c)
-    if c > b:
+        return c
+    elif c > b:
         return (a + b)
-    return (a*b - c)
+    else:
+        return (a * b - c)
