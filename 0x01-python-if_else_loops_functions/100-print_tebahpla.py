65a61fc4a1b1122e31049b6204f53238a1d3b80a
yapokoffiregis <regisbergan@gmail.com>
2022-08-23 21:23:50 -0700

    task number 14 commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# Range for Lowercase alphabet in reverse
-for i in range(122, 96, -1):
-    # We use this condition for search every number and print it
-    if i % 2 == 0:
-        print("{:c}".format(i), end="")
-    else:
-        # We print the left letters in uppercase
-        print("{:c}".format(i - 32), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
