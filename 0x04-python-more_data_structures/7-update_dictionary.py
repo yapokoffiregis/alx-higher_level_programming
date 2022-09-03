8ad7f91bf7fe581fdcccf1f399efcf6e4f437cbe
yapokoffiregis <regisbergan@gmail.com>
2022-09-02 19:59:57 -0700

    task 7 commit message

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-
-
-# update_dictionary - replaces or adds key/valu in a dictionary.
 def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value in a dictionary
+    ...
+
+    Parameters
+    ----------
+    a_dictionary : dictionary
+        the given dictionary
+    key : string
+        argument will be always a string
+    value : any type
+        argument will be any type
+
+    Return:
+        the dictionary with the new inserted value
+    """
+
     a_dictionary[key] = value
-    return a_dictionary
+    return (a_dictionary)
