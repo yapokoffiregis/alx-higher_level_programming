22ddae19207c54d42901bbaff9f61d4be87079fa
yapokoffiregis <regisbergan@gmail.com>
2022-09-21 21:45:32 -0700

    New file is added on 0x08-python_classes folder

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a rectangle class"""
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """this class represent a rectangle"""
+    """Represent a rectangle."""
     pass
